1c5a481d83ef19d327f17fb9a55d041303c97beb
tgorella <107557323+tgorella@users.noreply.github.com>
2023-03-12 23:19:02 +0300

    add deleteButton

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+// import { taskCompleted } from "./store/actions";
+import * as actions from "./store/actions";
+import { initiateStore } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = initiateStore();
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  const completeTask = (taskId) => {
+    store.dispatch(actions.taskCompleted(taskId));
+  };
+
+  const changeTitle = (taskId) => {
+    store.dispatch(actions.titleChanged(taskId));
+  };
+	const deleteTask = (taskId) => {
+		store.dispatch(actions.taskDeleted(taskId));
+	}
+  return (
+    <>
+      <h1>App</h1>
+      <ul>
+        {state.map((i) => {
+          return (
+            <li key={i.id}>
+              <p>{i.title}</p>
+              <p>{`Completed: ${i.completed}`}</p>
+              <button onClick={() => completeTask(i.id)}>Complete task</button>
+              <button onClick={() => changeTitle(i.id)}>Change title</button>
+              <button onClick={() => deleteTask(i.id)}>Delete task</button>
+
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
